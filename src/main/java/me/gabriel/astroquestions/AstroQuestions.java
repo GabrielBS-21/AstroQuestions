f3ac924e3d587abbe956dcbff3569aa0d556984d
Gabriel Santos <70302940+GabrielBS-21@users.noreply.github.com>
2020-10-31 00:44:25 -0300

    Init Project

---
+++ src/main/java/me/gabriel/astroquestions/AstroQuestions.java
@@ -0,0 +1,17 @@
+package me.gabriel.astroquestions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AstroQuestions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
